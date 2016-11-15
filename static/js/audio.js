cf47834b287736f80ae7bfb974ce2d1c7a5d87fb
geuis <geuis.teses@gmail.com>
2016-11-14 20:01:26 -0800

    more progress

--- static/js/audio.js
+++ static/js/audio.js
@@ -1,38 +1,35 @@
-// adult male will have a fundamental frequency from 85 to 180 Hz, and that of a typical adult female from 165 to 255 Hz
+// adult male will have a fundamental frequency from 85 to 180 Hz,
+// and that of a typical adult female from 165 to 255 Hz
 // const dB = 20 * Math.log10(Math.abs(data));
 class Iridium {
   constructor () {
     this.thresholdEl = document.querySelector('#threshold');
     this.delayEl = document.querySelector('#delay');
-    this.startEl = document.querySelector('#start');
+    this.recordToggleEl = document.querySelector('#record-toggle');
+    this.emitter = new EventEmitter();
     this.thresholdLevel = 1;
     this.delayTime = 1;
     this.started = false;
-    this.audioContext = null;
-    this.audioProcessor = null;
-    this.audioSource = null;
-    this.audioCache = [];
-    this.audioCacheLimit = 1000000;
-    this.audioCacheSize = 0;
+    this.inputAudioContext = new AudioContext();
+    this.inputAudioProcessor = null;
+    this.inputAudioSource = null;
+    this.inputAudioAnalyser = null;
+    this.inputAudioCache = [];
+    this.inputAudioCacheLimit = 1000000;
+    this.inputBufferSize = 512;
     this.isSpeaking = false;
-    this.emitter = new EventEmitter();
+    this.playbackTimeout = null;
+    this.playbackAudioContext = null;
 
     // reset ranges
     this.thresholdEl.value = this.thresholdLevel;
     this.delayEl.value = this.delayTime;
-
-    // setup input/change events on ranges to update their values when changed
-    document.querySelectorAll('input[type=range]')
-      .forEach(item => ['input', 'change'].forEach(str =>
-        item.addEventListener(str, ev =>
-          item.setAttribute('value', ev.target.value))));
 
     const eventList = {
       'startClicked': this.startClicked,
       'stopClicked': this.stopClicked,
       'thresholdChanged': this.thresholdChanged,
       'delayTimeChanged': this.delayTimeChanged,
-      'audioProcessData': this.audioProcessData,
       'speakingStatusChanged': this.speakingStatusChanged
     };
 
@@ -42,7 +39,13 @@ class Iridium {
     }
 
     // bind dom elements to trigger their events
-    this.startEl.addEventListener('click', () => this.emitter.emit(
+    // setup input/change events on ranges to update their values when changed
+    document.querySelectorAll('input[type=range]')
+      .forEach(item => ['input', 'change'].forEach(str =>
+        item.addEventListener(str, (ev) =>
+          item.setAttribute('value', ev.target.value))));
+
+    this.recordToggleEl.addEventListener('change', () => this.emitter.emit(
       this.started ? 'stopClicked' : 'startClicked'));
 
     // may be an issue where IE doesn't trigger 'input' events on range inputs
@@ -59,37 +62,47 @@ class Iridium {
   startClicked () {
     return navigator.mediaDevices.getUserMedia({audio: true})
       .then((stream) => {
-        if (!this.audioContext) {
-          const audioEl = document.createElement('audio');
-          audioEl.src = 'http://localhost:4000/martian_sample.mp3';
-//          audioEl.src = 'http://localhost:4000/martian_silence.mp3';
-//          audioEl.src = 'http://localhost:4000/bk_hach_001603_sample.mp3';
-//          audioEl.src = 'http://localhost:4000/bk_peng_002515_sample.mp3';
-//          audioEl.src = 'http://localhost:4000/bk_podm_000293_sample.mp3';
-          audioEl.loop = true;
+        if (!this.inputAudioSource) {
+//          const audioEl = document.createElement('audio');
+//          audioEl.src = 'http://localhost:4000/martian_sample.mp3';
+////          audioEl.src = 'http://localhost:4000/martian_silence.mp3';
+////          audioEl.src = 'http://localhost:4000/bk_hach_001603_sample.mp3';
+////          audioEl.src = 'http://localhost:4000/bk_peng_002515_sample.mp3';
+////          audioEl.src = 'http://localhost:4000/bk_podm_000293_sample.mp3';
+//          audioEl.loop = true;
+//          audioEl.play();
+//          this.inputAudioSource = this.inputAudioContext.createMediaElementSource(audioEl);
 
-          audioEl.play();
+          this.inputAudioSource = this.inputAudioContext
+            .createMediaStreamSource(stream);
 
-          this.audioContext = new AudioContext();
-//          this.audioSource = this.audioContext.createMediaStreamSource(stream);
-          this.audioSource = this.audioContext.createMediaElementSource(audioEl);
-          this.audioProcessor = this.audioContext
-            .createScriptProcessor(512, 1, 1);
+          this.inputAudioProcessor = this.inputAudioContext
+            .createScriptProcessor(this.inputBufferSize, 1, 1);
 
-          this.audioSource.connect(this.audioProcessor);
-          this.audioProcessor.connect(this.audioContext.destination);
+          this.inputAudioSource.connect(this.inputAudioProcessor);
+          this.inputAudioProcessor.connect(this.inputAudioContext.destination);
 
-          this.audioProcessor.onaudioprocess = this.audioProcessData.bind(this);
+          // copy audio data to cache
+          this.inputAudioProcessor.onaudioprocess = (audioEvent) => {
+            this.inputAudioCache.push(audioEvent.inputBuffer.getChannelData(0)
+              .slice(0));
+
+            // add conditional here to start removing oldest audio when some
+            // limit is reached. Unsure what the limit should be yet.
+            // console.log(this.inputAudioCache.length * this.inputBufferSize);
+          };
+
+          // analyser relies on audioSource to exist, so init must be run here
+          this.initCheckIsSpeaking();
+
+          this.initVisualizeAudio();
         }
-        
+
         this.started = true;
-        this.startEl.value = 'Stop';
 
-        if (this.audioContext.state === 'suspended') {
-          this.audioContext.resume();
+        if (this.inputAudioContext.state === 'suspended') {
+          this.inputAudioContext.resume();
         }
-
-        this.checkIsSpeaking();
       })
       .catch((err) => {
         console.log(err);
@@ -98,91 +111,147 @@ class Iridium {
   }
 
   stopClicked () {
-    this.startEl.value = 'Start';
     this.started = false;
-    this.audioContext.suspend();
-    this.audioCache = [];
+    this.inputAudioContext.suspend();
+    this.inputAudioCache = [];
   }
-  
+
   thresholdChanged (val) {
-    console.log('##', val);
+    this.thresholdLevel = val * 1;
   }
-  
+
   delayTimeChanged (val) {
-    console.log('##', val);
-  }
-  
-  audioProcessData (data) {
-    const channelCount = data.outputBuffer.numberOfChannels;
-
-    for (let i = 0; i < channelCount; i++) {
-      this.audioCache.push(data.inputBuffer.getChannelData(i).slice(0));
-    }
-
-    // add conditional here to start removing oldest audio when some limit
-    // is reached. Unsure what the limit should be yet.
-    // console.log(this.audioCache.length * 512);
-  }
-
-  checkIsSpeaking () {
-    const analyser = this.audioContext.createAnalyser();
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Float32Array(bufferLength);
-    let interval = null;
-
-    const threshold = .002;
-    const stackDepth = 10;
-
-    this.audioSource.connect(analyser);
-
-    if (this.started) {
-      let ampStack = [];
-
-      interval = setInterval(() => {
-        analyser.getFloatTimeDomainData(dataArray);
-
-        let maxAmplitude = 0;
-
-        dataArray.forEach((data, i) => {
-          const absData = Math.abs(data);
-
-          if (absData > maxAmplitude) {
-            maxAmplitude = absData;
-          }
-        });
-
-        ampStack.push(maxAmplitude);
-
-//        ampStack.push(Math.max(...))
-        if (ampStack.length > stackDepth) {
-          ampStack = ampStack.slice(1);
-//          ampStack.shift();
-        }
-
-        const runningAmpAvg = ampStack.reduce((p, c) => p + c, 0) / stackDepth;
-        
-        if (runningAmpAvg > threshold && !this.isSpeaking) {
-          this.isSpeaking = true;
-          this.emitter.emit('speakingStatusChanged');
-        }
-        
-        if (runningAmpAvg <= threshold && this.isSpeaking) {
-          this.isSpeaking = false;
-          this.emitter.emit('speakingStatusChanged');
-        }
-      }, 10);
-    } else {
-      this.isSpeaking = false;
-      clearInterval(interval);
-    }
+    this.delayTime = val * 1;
   }
 
   speakingStatusChanged () {
-    console.log(this.isSpeaking);
+    // if not speaking, start timeout for delayTime, then play in timeout
+    // if true, clear timeout and stop playback
+
+    if (!this.isSpeaking) {
+      this.playbackTimeout = setTimeout(() => {
+        this.playback();
+        this.stopClicked();
+      }, this.delayTime * 1000);
+    } else {
+      if (this.playbackAudioContext) {
+        this.playbackAudioContext.suspend();
+        this.playbackAudioContext = null;
+        this.startClicked();
+      }
+
+      clearTimeout(this.playbackTimeout);
+    }
   }
 
-  visualizeAudio () {
-  
+  playback () {
+    this.playbackAudioContext = new AudioContext();
+
+    // guessing the number of frames is equal to how many are stored in audioCache
+    const frameCount = this.inputAudioCache.length * this.inputBufferSize;
+    const playbackAudioBuffer = this.playbackAudioContext.createBuffer(1,
+      frameCount, this.playbackAudioContext.sampleRate);
+    const playbackAudioBufferChannel = playbackAudioBuffer.getChannelData(0);
+
+    // copy audio frame data to audioBuffer
+    let i = 0;
+    this.inputAudioCache.forEach((frame) => {
+      frame.forEach((data) => {
+        playbackAudioBufferChannel[i] = data;
+
+        i++;
+      });
+    });
+
+    this.inputAudioCache = [];
+
+    const source = this.playbackAudioContext.createBufferSource();
+
+    source.buffer = playbackAudioBuffer;
+    source.connect(this.playbackAudioContext.destination);
+
+    source.loop = true;
+    source.start();
+  }
+
+  initCheckIsSpeaking () {
+    this.inputAudioAnalyser = this.inputAudioContext.createAnalyser();
+    const dataArray = new Float32Array(this.inputAudioAnalyser.frequencyBinCount);
+    const threshold = .002;
+    const stackDepth = 10;
+
+    this.inputAudioSource.connect(this.inputAudioAnalyser);
+
+    let ampStack = [];
+
+    setInterval(() => {
+      if (this.started) {
+        this.inputAudioAnalyser.getFloatTimeDomainData(dataArray);
+
+        ampStack.push(Math.max.apply(Math, dataArray));
+
+        // remove oldest sample
+        if (ampStack.length > stackDepth) {
+          ampStack.shift();
+        }
+
+        const runningAmpAvg = ampStack.reduce((p, c) => p + c, 0) / stackDepth;
+
+        if (runningAmpAvg > (this.thresholdLevel * threshold) &&
+          !this.isSpeaking) {
+          this.isSpeaking = true;
+          this.emitter.emit('speakingStatusChanged');
+        }
+
+        if (runningAmpAvg <= (this.thresholdLevel * threshold) &&
+          this.isSpeaking) {
+          this.isSpeaking = false;
+          this.emitter.emit('speakingStatusChanged');
+        }
+      }
+    }, 10);
+  }
+
+  initVisualizeAudio () {
+    const canvas = document.querySelector('#display-canvas');
+    const canvasCtx = canvas.getContext('2d');
+    const dimensions = window.getComputedStyle(canvas);
+    const width = dimensions.width.replace('px', '') * 1;
+    const height = dimensions.height.replace('px', '') * 1;
+    const barWidth = width / this.inputAudioAnalyser.frequencyBinCount;
+
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+
+    canvasCtx.clearRect(0, 0, width, height);
+
+    canvasCtx.fillStyle = '#000';
+    canvasCtx.fillRect(0, 0, width, height);
+
+    const draw = () => {
+      if (this.started) {
+        const dataArray = new Float32Array(this.inputAudioAnalyser.frequencyBinCount);
+        this.inputAudioAnalyser.getFloatTimeDomainData(dataArray);
+
+        const bgColor = '#000';
+        canvasCtx.fillStyle = '#000';
+        canvasCtx.fillRect(0, 0, width, height);
+
+        let x = 0;
+
+        dataArray.forEach((data, i) => {
+          const barHeight = data * (height / 2);
+
+          canvasCtx.fillStyle = '#cc0000';
+          canvasCtx.fillRect(x, height / 2, barWidth, barHeight);
+          x += barWidth;
+        });
+      }
+
+      requestAnimationFrame(draw);
+    }
+    
+    draw();
   }
 };
 
@@ -193,34 +262,24 @@ window.addEventListener('load', () => {
 
 
 //window.addEventListener('load', () => {
-//  return;
-//
 //  navigator.mediaDevices.getUserMedia({audio: true})
 //    .then((stream) => {
-//      const audioSource = document.createElement('audio');
-//      audioSource.src = 'http://localhost:4000/martian_sample.mp3';
-////      audioSource.src = 'http://localhost:4000/martian_silence.mp3';
-////      audioSource.src = 'http://localhost:4000/bk_hach_001603_sample.mp3';
-////      audioSource.src = 'http://localhost:4000/bk_peng_002515_sample.mp3';
-////      audioSource.src = 'http://localhost:4000/bk_podm_000293_sample.mp3';
-////      audioSource.loop = true;
-////setTimeout(() => audioSource.play(), 0);
-//
 //      const context = new AudioContext();
 //      // microphone
-////      const source = context.createMediaStreamSource(stream);
-//      const source = context.createMediaElementSource(audioSource);
-//      const processor = context.createScriptProcessor(512, 1, 1);
+//      const source = context.createMediaStreamSource(stream);
+//      const processor = context.createScriptProcessor(1024, 1, 1);
 //
 //      source.connect(processor);
 //      processor.connect(context.destination);
 //
-//      const audioCache = [];
+////      const audioCache = [];
 //
 //      const start = Date.now();
 //      const delay = 0;
 //
-//// live output
+//      let len = 0;
+//
+//      // live output
 //      processor.onaudioprocess = (data) => {
 //        const channelCount = data.outputBuffer.numberOfChannels;
 //
@@ -230,9 +289,16 @@ window.addEventListener('load', () => {
 //
 //          for (let j = 0; j < outputChannelData.length; j++) {
 //            outputChannelData[j] = inputChannelData[j];
+//            len += inputChannelData[j];
 //          }
 //        }
+//        
+////        console.log(len);
+//        len = 0;
 //      };
+//    })
+//});
+
 //
 //
 //// delayed output
